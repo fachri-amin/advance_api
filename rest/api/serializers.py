dee1ee65c4b98888e30a534e24fd9abb30601256
fachri-amin <fachri.amin@gmail.com>
2020-06-15 20:21:22 +0700

    adding addtional field on serializers

--- rest/api/serializers.py
+++ rest/api/serializers.py
@@ -4,6 +4,12 @@ from rest.models import Blog
 
 
 class BlogSerializer(serializers.ModelSerializer):
+
+    author = serializers.SerializerMethodField('get_username')
+
     class Meta:
         model = Blog
-        fields = ['title', 'body']
+        fields = ['title', 'body', 'author']
+
+    def get_username(self, blog):
+        return blog.author.username
